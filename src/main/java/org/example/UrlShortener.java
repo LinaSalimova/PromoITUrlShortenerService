a2d10bf32fa33c3762bc74a35011d76e0839c4b6
Alina Salimova <106020410+LinaSalimova@users.noreply.github.com>
2025-01-13 16:42:26 +0300

    Update UrlShortener.java

--- src/main/java/org/example/UrlShortener.java
+++ src/main/java/org/example/UrlShortener.java
@@ -1,7 +1,16 @@
 package org.example;
 
 import java.util.UUID;
-
+/**
+ * Интерфейс для генерации коротких ссылок.
+ */
 public interface UrlShortener {
+     /**
+     * Создает короткую ссылку на основе длинного URL и идентификатора пользователя.
+     *
+     * @param longUrl длинный URL для сокращения
+     * @param userId идентификатор пользователя
+     * @return короткая ссылка
+     */
     String shorten(String longUrl, UUID userId);
 }
