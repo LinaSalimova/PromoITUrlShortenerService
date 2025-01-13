21c298b293b1185b45d1af3b46b0dc971e09696a
Alina Salimova <106020410+LinaSalimova@users.noreply.github.com>
2025-01-13 17:09:10 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,13 @@
 package org.example;
-
+/**
+ * Главный класс для демонстрации работы сервиса сокращения ссылок.
+ */
 public class Main {
+     /**
+     * Точка входа в программу.
+     *
+     * @param args аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
         UrlShortener shortener = new BaseUrlShortener();
         UrlManager urlManager = new InMemoryUrlManager();
