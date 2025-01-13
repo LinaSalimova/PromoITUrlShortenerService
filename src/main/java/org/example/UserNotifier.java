ed7c8840c256b1abfed3c0e9a51e25764fec0c3b
Alina Salimova <106020410+LinaSalimova@users.noreply.github.com>
2025-01-13 16:53:02 +0300

    Update UserNotifier.java

--- src/main/java/org/example/UserNotifier.java
+++ src/main/java/org/example/UserNotifier.java
@@ -1,8 +1,22 @@
 package org.example;
 
 import java.util.UUID;
-
+/**
+ * Интерфейс для уведомления пользователей о событиях, связанных с их ссылками.
+ */
 public interface UserNotifier {
+     /**
+     * Уведомляет пользователя об истечении срока действия ссылки.
+     *
+     * @param userId идентификатор пользователя
+     * @param shortUrl короткая ссылка
+     */
     void notifyLinkExpired(UUID userId, String shortUrl);
+    /**
+     * Уведомляет пользователя о достижении лимита переходов по ссылке.
+     *
+     * @param userId идентификатор пользователя
+     * @param shortUrl короткая ссылка
+     */
     void notifyLinkLimitReached(UUID userId, String shortUrl);
 }
