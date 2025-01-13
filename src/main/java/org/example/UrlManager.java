33a92113b853ca868766f874030b5f89d40d0687
Alina Salimova <106020410+LinaSalimova@users.noreply.github.com>
2025-01-13 16:50:38 +0300

    Update UrlManager.java

--- src/main/java/org/example/UrlManager.java
+++ src/main/java/org/example/UrlManager.java
@@ -1,11 +1,38 @@
 package org.example;
 
 import java.util.Map;
-
+/**
+ * Интерфейс для управления сокращенными ссылками.
+ */
 public interface UrlManager {
+    /**
+     * Добавляет новую сокращенную ссылку в базу данных.
+     *
+     * @param shortUrl короткая ссылка
+     * @param urlInfo информация о ссылке
+     */
     void addUrl(String shortUrl, UrlInfo urlInfo);
+     /**
+     * Получает информацию о ссылке по её короткому варианту.
+     *
+     * @param shortUrl короткая ссылка
+     * @return информация о ссылке или null, если ссылка не найдена
+     */
     UrlInfo getUrlInfo(String shortUrl);
+    /**
+     * Увеличивает счетчик переходов по ссылке.
+     *
+     * @param shortUrl короткая ссылка
+     */
     void incrementClicks(String shortUrl);
+        /**
+     * Удаляет все истекшие ссылки.
+     */
     void removeExpiredUrls();
+    /**
+     * Возвращает все сохраненные ссылки.
+     *
+     * @return карта всех сохраненных ссылок
+     */
     Map<String, UrlInfo> getAllUrls();
 }
